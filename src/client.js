21cf996207d87e4c097b5efa0a54c093feb2f388
Ciro Cesar Maciel <ciro.maciel@c37.co>
2017-10-21 11:33:12 -0200

    - router

--- src/client.js
+++ src/client.js
@@ -5,9 +5,11 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import Main from './main';
 
+console.log(process.env.PUBLIC_URL);
+
 // https://github.com/facebookincubator/create-react-app/issues/1765
 render(
-	<HashRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<Main />
-	</HashRouter>,
+	</BrowserRouter>,
 	document.getElementById('container'));
